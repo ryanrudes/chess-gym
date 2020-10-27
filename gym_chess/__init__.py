454cdf72ad2891d43e186c5edafd10e4362181e6
Ryan Rudes <ryanrudes@gmail.com>
2020-10-27 15:08:18 -0400

    Update __init__.py

--- gym_chess/__init__.py
+++ gym_chess/__init__.py
@@ -2,7 +2,7 @@ from gym.envs.registration import register
 
 register(
     id='Chess-v0',
-    entry_point='gym_chess.envs:ChessEnv'
+    entry_point='gym_chess.envs:ChessEnv',
     kwargs={'chess960': False}
 )
 
