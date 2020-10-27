2f1de4ffceca039836db4500daa91a073d181a1b
Ryan Rudes <ryanrudes@gmail.com>
2020-10-27 13:10:32 -0400

    Update __init__.py

--- gym_chess/__init__.py
+++ gym_chess/__init__.py
@@ -2,6 +2,5 @@ from gym.envs.registration import register
 
 register(
     id='Chess-v0',
-    entry_point='gym_minecraft.envs:ChessEnv',
-    kwargs={'mission_file': 'default_world_1.xml'}
+    entry_point='gym_chess.envs:ChessEnv'
 )
