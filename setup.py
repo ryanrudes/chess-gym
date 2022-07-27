eaea7690fbbe16ba271c12f95ce7d75097d0a2cb
Ryan Rudes <ryanrudes@gmail.com>
2022-07-27 16:54:49 -0400

    Incremented patch version number

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='chess_gym',
-      version='0.0.3',
+      version='0.0.4',
       description='OpenAI Gym environment for Chess, using the game engine of the python-chess module',
       url='https://github.com/Ryan-Rudes/chess-gym',
       author='Ryan Rudes',
