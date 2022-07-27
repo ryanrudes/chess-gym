a4c1f605853e158b38ab5e469c7da3aa4c580afc
Ryan Rudes <ryanrudes@gmail.com>
2022-07-27 16:53:31 -0400

    Fixed https://github.com/ryanrudes/chess-gym/issues/2

--- chess_gym/__init__.py
+++ chess_gym/__init__.py
@@ -2,12 +2,12 @@ from gym.envs.registration import register
 
 register(
     id='Chess-v0',
-    entry_point='gym_chess.envs:ChessEnv',
+    entry_point='chess_gym.envs:ChessEnv',
     kwargs={'chess960': False}
 )
 
 register(
     id='Chess960-v0',
-    entry_point='gym_chess.envs:ChessEnv',
+    entry_point='chess_gym.envs:ChessEnv',
     kwargs={'chess960': True}
 )
